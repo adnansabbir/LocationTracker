a34f95d6c86a57feb2d02ca7aad1e42d63da7eca
adnansabbir <adnan.sabbir2012@gmail.com>
2019-04-14 03:31:16 +0600

    Send request to api endpoints

--- locationTracker.py
+++ locationTracker.py
@@ -1,6 +1,9 @@
 from tkinter import *
-from PIL import Image, ImageTk
+import socket
+import requests
 
+
+url = ""
 
 # Here, we are creating our class, Window, and inheriting from the Frame
 # class. Frame is a class from the tkinter module. (see Lib/tkinter/__init__)
@@ -13,7 +16,6 @@ class Window(Frame):
 
         # reference to the master widget, which is the tk window
         self.master = master
-
         # with that, we want to then run init_window, which doesn't yet exist
         self.init_window()
 
@@ -22,41 +24,57 @@ class Window(Frame):
         # changing the title of our master widget
         self.master.title("Location Tracker")
 
-        # allowing the widget to take the full space of the root window
+        # # allowing the widget to take the full space of the root window
         self.pack(fill=BOTH, expand=1)
-
-        # creating a menu instance
+        #
+        # # creating a menu instance
         menu = Menu(self.master)
         self.master.config(menu=menu)
-
-        # create the file object)
+        #
+        # # create the file object)
         file = Menu(menu)
-
-        # adds a command to the menu option, calling it exit, and the
-        # command it runs on event is client_exit
+        #
+        # # adds a command to the menu option, calling it exit, and the
+        # # command it runs on event is client_exit
         file.add_command(label="Exit", command=self.client_exit)
-
-        # added "file" to our menu
+        #
+        # # added "file" to our menu
         menu.add_cascade(label="File", menu=file)
 
-        # create the file object)
-        edit = Menu(menu)
+        # Label(self, text='Enter URL').grid(row=0)
+        # e1 = Entry(self)
+        # e1.grid(row=0, column=1)
 
-        # adds a command to the menu option, calling it exit, and the
-        # command it runs on event is client_exit
-        edit.add_command(label="Show Text", command=self.showText)
+        e1 = self.create_entry("ID", 0)
+        e2 = self.create_entry("URL", 1)
 
-        # added "file" to our menu
-        menu.add_cascade(label="Edit", menu=edit)
+        def print_data():
+            url = e2.get()
+            r = requests.post(url, data={"ID": e1.get(), "IP" : self.get_ip()})
+            print(r.url, r.status_code, r.reason)
 
-    def showText(self):
-        text = Label(self, text="Hey there good lookin!")
-        text.pack()
+        Button(self, text="Save", command=print_data).grid(row=4)
 
     def client_exit(self):
         exit()
 
+    def create_entry(self, label, row):
+        Label(self, text=label).grid(row=row)
+        e = Entry(self)
+        e.grid(row=row, column = 1)
+        return e
 
+    def get_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            IP = s.getsockname()[0]
+        except:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+        return IP
 
 root = Tk()
 #size of the window
